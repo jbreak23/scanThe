301c334a41dcade3e3f38856e3cee61ecbb96bfa
Jonathan Breaker <jbreak23@gmail.com>
2025-05-05 21:33:06 -0400

    Initial Commit

---
+++ scanThe/scanTheApp.swift
@@ -0,0 +1,17 @@
+//
+//  scanTheApp.swift
+//  scanThe
+//
+//  Created by Jonathan Breaker on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct scanTheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
